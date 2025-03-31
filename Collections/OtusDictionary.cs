4691e9bfda163b4a9640b83feb8d578ab90a1a0b
VtyurinaSI <ustaaaaaalll@gmail.com>
2025-03-31 18:00:28 +0300

    Добавьте файлы проекта.

---
+++ Collections/OtusDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class OtusDictionary
+    {
+        public OtusDictionary()
+        {
+            capacity = 32;
+            keys = new int[capacity];
+            values = new string[capacity];
+        }
+        private int capacity;
+        private int[] keys;
+        private string[] values;
+        public string Get(int key)
+        {
+            int index = GetIndex(key, capacity);
+            return values[index];
+        }
+        public void Add(int key, string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException();
+
+            while (true)
+            {
+                int index = GetIndex(key, capacity);
+                if (values[index] == null || keys[index] == key)
+                {
+                    values[index] = value;
+                    keys[index] = key;
+                    return;
+                }
+                else Resize();
+            }
+        }
+
+        private int GetIndex(int key, int capacity)
+        {
+            int index = key % capacity;
+            if (index < 0) index += capacity;
+            return index;
+        }
+
+        private void Resize()
+        {
+            int oldcapacity = capacity;
+            capacity *= 2;
+            int[] _newkeys = new int[capacity];
+            string[] _newvalues = new string[capacity];
+            for (int i = 0; i < oldcapacity; i++)
+            {
+                if (values[i] == null) continue;
+                int index = GetIndex(keys[i],oldcapacity);
+                _newkeys[index] = keys[i];
+                _newvalues[index] = values[i];
+            }
+            keys = _newkeys;
+            values = _newvalues;
+        }
+
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            OtusDictionary dict = new();
+            
+            dict.Add(1,"Сережа");
+            dict.Add(1, "Саша");
+            dict.Add(-1, "Ваня");
+            dict.Add(33, "Василий");
+            //dict.Add(3, null);
+
+            dict.Add(6, "Сбрил брови сыну");
+            dict.Add(11, "Зачем");
+            dict.Add(16, "Не объяснив причину");
+            
+
+            Console.WriteLine(dict.Get(1));
+            Console.WriteLine(dict.Get(6));
+            Console.WriteLine(dict.Get(11));
+            Console.WriteLine(dict.Get(16));
+
+            Console.WriteLine($"Несуществующий элемент: {dict.Get(21)}");
+            Console.ReadKey();
+
+        }
+    }
+}
